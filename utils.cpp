4f3ea23d4dafeebd02118bed57b636fb8b7fff66
Kaito <kaaitokid@gmail.com>
2015-10-03 20:08:48 +0200

    added functions to utils

--- utils.cpp
+++ utils.cpp
@@ -13,3 +13,23 @@ int myStoi(std::string s) {
 		x = 10*x+(s[i]-'0');
 	return x;
 }
+
+float getAngle(const sf::Vector2f &orig,const sf::Vector2f &des) {
+    return std::atan2(des.y - orig.y, des.x - orig.x)*180/(M_PI);
+}
+
+float getModule(const sf::Vector2f &orig, const sf::Vector2f &des) {
+    return std::sqrt(std::pow(std::abs(des.x-orig.x), 2) + std::pow(std::abs(des.y-orig.y), 2));
+}
+
+float radToAngle(float rad) {
+    return rad * 180 / M_PI;
+}
+
+float floatangleToRad(float angle) {
+    return angle * M_PI / 180;
+}
+
+bool isCollisioning(sf::Vector2f point1, float radius1, sf::Vector2f point2, float radius2) {
+    return (getModule(point1,point2) < radius1+radius2);
+}
