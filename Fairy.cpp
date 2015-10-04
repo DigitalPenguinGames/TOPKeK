0b4b5c79c4b33782b8e84c52f5db79b1869a97a4
Kaito <kaaitokid@gmail.com>
2015-10-04 17:03:19 +0200

    added effect

---
+++ Fairy.cpp
@@ -0,0 +1,5 @@
+#include "Fairy.hpp"
+
+Fairy::Fairy()
+{
+}
---
+++ Fairy.hpp
@@ -0,0 +1,26 @@
+#ifndef __FAIRY_HPP__
+#define __FAIRY_HPP__
+
+#include "utils.hpp"
+#include "Resources.hpp"
+#include "Collisionable.hpp"
+
+class Fairy : public Collisionable {
+
+public:
+
+    Fairy();
+/*    ~Fairy();
+
+    void update();
+
+    void draw();
+
+private:
+
+    Effect _effect;
+  */
+
+};
+
+#endif
