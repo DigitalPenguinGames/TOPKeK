c94b7be8ea968348e610db0b031982555016f703
Kaito <kaaitokid@gmail.com>
2015-10-06 22:34:18 +0200

    hardcoded distances from sword

--- Player.cpp
+++ Player.cpp
@@ -46,16 +46,16 @@ void Player::draw(sf::RenderTarget* w) {
     // Draw Sword
     if (_attacking) {
         sf::Vector2f size = sf::Vector2f(_sprite.getGlobalBounds().width,_sprite.getGlobalBounds().height);
-        float offset = size.x/8;
+
         _sword.setPosition(sf::Vector2f(        // V posar la posició del player
-                        _sword.getPosition().x + ((size.x/2+offset) * (_dir == directions::up))
-                                               + ((size.x/2-offset) * (_dir == directions::down))
-                                               + ((size.x-offset)   * (_dir == directions::right))
-                                               + (offset/2            * (_dir == directions::left)),
-                        _sword.getPosition().y + ((size.y/2+2*offset) * (_dir == directions::right))
-                                               + ((size.y/2-2*offset) * (_dir == directions::left))
-                                               + ((size.y-offset) * (_dir == directions::down))
-                                               + (offset          * (_dir == directions::up))
+                        _sword.getPosition().x + ((size.x/2+4) * (_dir == directions::up))
+                                               + ((size.x/2-2) * (_dir == directions::down))
+                                               + ((size.x-5)   * (_dir == directions::right))
+                                               + (4            * (_dir == directions::left)),
+                        _sword.getPosition().y + ((size.y/2+4) * (_dir == directions::right))
+                                               + ((size.y/2-3) * (_dir == directions::left))
+                                               + ((size.y-2-3) * (_dir == directions::down))
+                                               + (6           * (_dir == directions::up))
                         ));
 
         _sword.draw(w);
